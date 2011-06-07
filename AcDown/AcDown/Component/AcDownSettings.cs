c1d1c8f1c5b9ffb48c1a118dc0fb58b9221abe45
Kaedei <kaedei@users.noreply.github.com>
2011-06-07 12:28:34 +0000

    新增: 主界面新增了优酷网链接 主界面快捷搜索支持搜酷搜索 新增了插件的启用/禁用设置 修改: UI的一些小改动

--- AcDown/AcDown/Component/AcDownSettings.cs
+++ AcDown/AcDown/Component/AcDownSettings.cs
@@ -15,134 +15,129 @@ using System.Diagnostics;
 
 namespace Kaedei.AcDown
 {
-	 [Serializable]
-	 public class AcDownSettings
-	 {
-		  //程序设置
-		 public bool WatchClipboardEnabled = true; //监视剪贴板
-		  public bool DownSub=true  ; //下载字幕
-		  public string SavePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); //保存文件夹
-		  public bool AutoDownAllSection ; //自动下载所有分段
-		  public Int32 CacheSize=1; //缓存文件
-		  public bool OpenFolderAfterComplete; //完成后打开文件夹
-		  public bool PlaySound=true; //播放声音
-		  public string SoundFile = ""; //声音文件路径 (wav格式)
-		  public bool EnableLog = false; //运行记录日志
-		  public bool AutoCheckUrl = true; //自动检查URL
-		  //public bool ShowTrayIcon = true; //显示托盘图标
-		  public bool ShowBigStartButton = true; //显示大按钮
-		  public Int32 MaxRunningTaskCount = 2; //最多同时运行任务数量
-		  public bool SaveWhenAbort = true; //任务停止或错误退出时保存已经下载的部分
-		  //public string SearchEngine = "Acfun站内搜索 - Google"; //默认的搜索引擎。还可以是Google或Baidu
-		  public string SearchQuery = @""; //搜索url
-		  public bool EnableWindows7Feature = true; //允许使用Windows7特性
-		  public bool DeleteTaskAndFile = false; //删除任务的同时删除文件
-		  
+   [Serializable]
+   public class AcDownSettings
+   {
+      //程序设置
+      public bool WatchClipboardEnabled = true; //监视剪贴板
+      public bool DownSub = true; //下载字幕
+      public string SavePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); //保存文件夹
+      public bool AutoDownAllSection; //自动下载所有分段
+      public Int32 CacheSize = 1; //缓存文件
+      public bool OpenFolderAfterComplete; //完成后打开文件夹
+      public bool PlaySound = true; //播放声音
+      public string SoundFile = ""; //声音文件路径 (wav格式)
+      public bool EnableLog = false; //运行记录日志
+      public bool AutoCheckUrl = true; //自动检查URL
+      public bool ShowBigStartButton = true; //显示大按钮
+      public Int32 MaxRunningTaskCount = 2; //最多同时运行任务数量
+      public bool SaveWhenAbort = true; //任务停止或错误退出时保存已经下载的部分
+      public string SearchQuery = @""; //搜索url
+      public bool EnableWindows7Feature = true; //允许使用Windows7特性
+      public bool DeleteTaskAndFile = false; //删除任务的同时删除文件
 
-	  }
+      //启用插件
+      public bool Plugin_Enable_Acfun = true;
+      public bool Plugin_Enable_Bilibili = true;
+      public bool Plugin_Enable_Tudou = true;
+      public bool Plugin_Enable_Youku = true;
+   }
 
-	 public static class Config
-	 {
-		  public static AcDownSettings setting { get; set; }
-		  
-		  /// <summary>
-		  /// 保存设置
-		  /// </summary>
-		  public static void SaveSettings()
-		  {
-			  
-					 //取得APPDATA路径名称
-					 string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-					 path = Path.Combine(path, @"Kaedei\AcDown\");
+   public static class Config
+   {
+      public static AcDownSettings setting { get; set; }
 
-					 if (!Directory.Exists(path))
-					 {
-						  //如果目录不存在则创建
-						  Directory.CreateDirectory(path);
-					 }
+      /// <summary>
+      /// 保存设置
+      /// </summary>
+      public static void SaveSettings()
+      {
 
-					 //序列化设置
-					 using (FileStream fs = new FileStream(path + @"config.xml", FileMode.Create))
-					 {
-						  XmlSerializer formatter = new XmlSerializer(typeof(AcDownSettings));
-						  formatter.Serialize(fs, setting);
-					 }
-				
-		  }
+         //取得APPDATA路径名称
+         string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+         path = Path.Combine(path, @"Kaedei\AcDown\");
 
-		  /// <summary>
-		  /// 读取设置
-		  /// </summary>
-		  /// <returns></returns>
-		  public static AcDownSettings LoadSettings()
-		  {
-			  try
-			  {
-				  AcDownSettings s;
-				  string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-				  path = Path.Combine(path, @"Kaedei\AcDown\config.xml");
-				  using (FileStream fs = new FileStream(path, FileMode.Open))
-				  {
-					  XmlSerializer formatter = new XmlSerializer(typeof(AcDownSettings));
-					  s = (AcDownSettings)formatter.Deserialize(fs);
-				  }
+         if (!Directory.Exists(path))
+         {
+            //如果目录不存在则创建
+            Directory.CreateDirectory(path);
+         }
 
-				  if (s != null)
-					  setting = s;
-				  else
-					  throw new Exception();
-			  }
-			  catch
-			  {
-				  setting = new AcDownSettings();
-				  SaveSettings();
-			  }
-			  finally
-			  {
-				  GlobalSettings.GetSettings().CacheSizeMb = setting.CacheSize;
-				  GlobalSettings.GetSettings().DownSub = setting.DownSub;
-				  //GlobalSettings.GetSettings().SaveFileWhenAbort = setting.SaveWhenAbort;
-			  }
-				return setting; 
-		  }
+         //序列化设置
+         using (FileStream fs = new FileStream(path + @"config.xml", FileMode.Create))
+         {
+            XmlSerializer formatter = new XmlSerializer(typeof(AcDownSettings));
+            formatter.Serialize(fs, setting);
+         }
 
-	 
+      }
+
+      /// <summary>
+      /// 读取设置
+      /// </summary>
+      /// <returns></returns>
+      public static AcDownSettings LoadSettings()
+      {
+         try
+         {
+            AcDownSettings s;
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            path = Path.Combine(path, @"Kaedei\AcDown\config.xml");
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+               XmlSerializer formatter = new XmlSerializer(typeof(AcDownSettings));
+               s = (AcDownSettings)formatter.Deserialize(fs);
+            }
+
+            if (s != null)
+               setting = s;
+            else
+               throw new Exception();
+         }
+         catch
+         {
+            setting = new AcDownSettings();
+            SaveSettings();
+         }
+         finally
+         {
+            GlobalSettings.GetSettings().CacheSizeMb = setting.CacheSize;
+            GlobalSettings.GetSettings().DownSub = setting.DownSub;
+            //GlobalSettings.GetSettings().SaveFileWhenAbort = setting.SaveWhenAbort;
+         }
+         return setting;
+      }
 
 
-				/// <summary>
-		  /// 判断当前系统是否为Windows7
-		  /// </summary>
-		  /// <returns></returns>
-		 [DebuggerNonUserCode()]
-		 public static bool IsWindows7()
-		  {
-				if (Environment.OSVersion.Version.Major == 6 && Environment.OSVersion.Version.Minor >= 1)
-					 return true;
-				else
-					 return false;
-		  }
-				[DebuggerNonUserCode()]
-		  public static bool IsWindowsVista()
-		  {
-				if (Environment.OSVersion.Version.Major == 6 && Environment.OSVersion.Version.Minor == 0)
-					 return true;
-				else
-					 return false;
-		  }
-				[DebuggerNonUserCode()]
-		  public static bool IsWindowsVistaOr7()
-		  {
-				if (Environment.OSVersion.Version.Major == 6 )
-					 return true;
-				else
-					 return false;
-		  }
-	 }
-	 //[Serializable]
-	 //class CheckNewXml
-	 //{
-	 //    public Version LastestVersion;
-	 //    public DateTime PublishDate;
-	 //    public string Describe;
-	 //}
+
+
+      /// <summary>
+      /// 判断当前系统是否为Windows7
+      /// </summary>
+      /// <returns></returns>
+      [DebuggerNonUserCode()]
+      public static bool IsWindows7()
+      {
+         if (Environment.OSVersion.Version.Major == 6 && Environment.OSVersion.Version.Minor >= 1)
+            return true;
+         else
+            return false;
+      }
+      [DebuggerNonUserCode()]
+      public static bool IsWindowsVista()
+      {
+         if (Environment.OSVersion.Version.Major == 6 && Environment.OSVersion.Version.Minor == 0)
+            return true;
+         else
+            return false;
+      }
+      [DebuggerNonUserCode()]
+      public static bool IsWindowsVistaOr7()
+      {
+         if (Environment.OSVersion.Version.Major == 6)
+            return true;
+         else
+            return false;
+      }
+   }
 }
