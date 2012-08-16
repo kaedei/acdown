a7251dd930a7bca45bdbfafb3ede636e1c7dd098
Kaedei <kaedei@users.noreply.github.com>
2012-08-16 08:39:33 +0000

    修复: 修复了某些情况下自动更新不成功的问题 修复了插件管理窗口的报错问题

--- AcDown/AcDown/UI/Components/PluginSettings.cs
+++ AcDown/AcDown/UI/Components/PluginSettings.cs
@@ -9,6 +9,7 @@ using Kaedei.AcDown.Core;
 using Kaedei.AcDown.Interface;
 using System.Collections.ObjectModel;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace Kaedei.AcDown.UI.Components
 {
@@ -58,14 +59,24 @@ namespace Kaedei.AcDown.UI.Components
 		//检查插件是否支持删除和属性
 		private void lsv_SelectedIndexChanged(object sender, EventArgs e)
 		{
+			btnSupport.Enabled = false;
 			btnProperty.Enabled = false;
 			if (lsv.SelectedItems.Count == 1)
 			{
 				var lvi = lsv.SelectedItems[0];
 				var plugin = (IPlugin)lvi.Tag;
-				if (plugin.Feature.ContainsKey("ConfigForm"))
+				if (plugin.Feature != null && plugin.Feature.ContainsKey("ConfigForm"))
 				{
 					btnProperty.Enabled = true;
+				}
+				object[] types = plugin.GetType().GetCustomAttributes(typeof(AcDownPluginInformationAttribute), true);
+				var attrib = (AcDownPluginInformationAttribute)types[0];
+				if (!string.IsNullOrEmpty(attrib.SupportUrl))
+				{
+					if (attrib.SupportUrl.StartsWith("http://", StringComparison.CurrentCultureIgnoreCase))
+					{
+						btnSupport.Enabled = true;
+					}
 				}
 			}
 		}
@@ -79,6 +90,19 @@ namespace Kaedei.AcDown.UI.Components
 			this.Invoke(method);
 			//保存设置
 			CoreManager.PluginManager.SaveConfiguration(plugin);
+		}
+
+		private void btnSupport_Click(object sender, EventArgs e)
+		{
+			var lvi = lsv.SelectedItems[0];
+			var plugin = (IPlugin)lvi.Tag;
+			object[] types = plugin.GetType().GetCustomAttributes(typeof(AcDownPluginInformationAttribute), true);
+			var attrib = (AcDownPluginInformationAttribute)types[0];
+			try
+			{
+				Process.Start(attrib.SupportUrl);
+			}
+			catch { }
 		}
 
 	}
