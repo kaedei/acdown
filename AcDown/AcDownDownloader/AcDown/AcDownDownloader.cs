071b7c2fa8f4637cb69379229535fb14ff7cb143
kaedei_cp <SND\kaedei_cp>
2012-12-04 03:22:53 +0000

    版本号更新为 v4.4.0.1204 添加Acdown UI组件下载插件

---
+++ AcDown/AcDownDownloader/AcDown/AcDownDownloader.cs
@@ -0,0 +1,71 @@
+﻿using Kaedei.AcDown.Interface;
+using Kaedei.AcDown.Interface.Downloader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Kaedei.AcDown.Downloader.AcDown
+{
+	public class AcDownDownloader : CommonDownloader
+	{
+		public override bool Download()
+		{
+			string FlvBind_exe = "https://acdown.svn.codeplex.com/svn/AcDown/FlvCombine/FlvCombine/FlvBind.exe";
+			string FlvLib_dll = "https://acdown.svn.codeplex.com/svn/AcDown/FlvCombine/FlvCombine/FLVLib.dll";
+			string FFMpeg_exe = "";
+			string Libfaac_dll = "";
+
+			string appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+			string folder = Path.Combine(appdata, "Kaedei" + Path.DirectorySeparatorChar + "FlvCombine" + Path.DirectorySeparatorChar);
+
+			Info.Title = "AcDown UI组件";
+
+			NewPart(1, 4);
+			TipText("正在下载Flv合并组件(1/2)");
+			p = new DownloadParameter()
+			{
+				 Url = FlvBind_exe,
+				 FilePath = Path.Combine(folder,"FlvBind.exe"),
+				 Proxy = Info.Proxy,
+				 Referer = "http://acdown.codeplex.com"
+			};
+			DownloadFile();
+
+			NewPart(2, 4);
+			TipText("正在下载Flv合并组件(2/2)");
+			p = new DownloadParameter()
+			{
+				Url = FlvLib_dll,
+				FilePath = Path.Combine(folder, "FlvLib.dll"),
+				Proxy = Info.Proxy,
+				Referer = "http://acdown.codeplex.com"
+			};
+			DownloadFile();
+
+			NewPart(3, 4);
+			TipText("正在下载高级视频合并组件(1/2)");
+			p = new DownloadParameter()
+			{
+				Url = FFMpeg_exe,
+				FilePath = Path.Combine(folder, "ffmpeg.exe"),
+				Proxy = Info.Proxy,
+				Referer = "http://acdown.codeplex.com"
+			};
+			DownloadFile();
+
+			NewPart(4, 4);
+			TipText("正在下载高级视频合并组件(2/2)");
+			p = new DownloadParameter()
+			{
+				Url = Libfaac_dll,
+				FilePath = Path.Combine(folder, "libfaac.dll"),
+				Proxy = Info.Proxy,
+				Referer = "http://acdown.codeplex.com"
+			};
+			DownloadFile();
+
+			return true;
+		}
+	}
+}
---
+++ AcDown/AcDownDownloader/AcDown/AcDownPlugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kaedei.AcDown.Interface;
+
+namespace Kaedei.AcDown.Downloader.AcDown
+{
+	[AcDownPluginInformation("AcDownPlugin", "AcDown UI组件下载插件", "Kaedei", "4.4.0.1204", "AcDown UI插件(视频合并等)下载插件", "http://blog.sina.com.cn/kaedei")]
+	public class AcDownPlugin : IPlugin
+	{
+		private string[] supportedUrl = { "视频合并插件" };
+		public IDownloader CreateDownloader()
+		{
+			return new AcDownDownloader();
+		}
+
+		public bool CheckUrl(string url)
+		{
+			foreach (string item in supportedUrl)
+			{
+				if (item.Equals(url, StringComparison.CurrentCultureIgnoreCase))
+					return true;
+			}
+			return false;
+		}
+
+		public string GetHash(string url)
+		{
+			return url;
+		}
+
+		public Dictionary<string, object> Feature { get; }
+
+		public SerializableDictionary<string, string> Configuration { get; set; }
+
+	}
+}
